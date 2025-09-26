cb8d3078856fa1794bde15af0bf272fb45d68389
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-09-26 18:12:12 +0530

    commit

---
+++ Testng/26-9/Ex2.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+public class Ex2 
+{
+	@Test
+	public void a()
+	{
+		System.out.println("A Called");
+	}
+	
+	@Test
+	public void c()
+	{
+		System.out.println("C Called");
+	}
+	
+	@Test
+	public void b()
+	{
+		System.out.println("B Called");
+	}
+	
+}
