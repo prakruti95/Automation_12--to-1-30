f3e238b49a5e5c0e739aa263d5b1f966a536dea2
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-10-06 17:53:31 +0530

    commit

---
+++ error/parameters/LoginTest.java
@@ -0,0 +1,28 @@
+package parameters;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import util.Driverconnection;
+
+public class LoginTest 
+{
+	
+	@Parameters({"uname","pass"})
+	@Test
+	public void login(String uname, String pass)
+	{
+		WebDriver driver = Driverconnection.connect();
+		driver.get("https://www.facebook.com/");
+		
+//		WebElement u =  driver.findElement(By.id("email"));
+//		u.clear();
+//		u.sendKeys(username);
+//		WebElement p =  driver.findElement(By.id("pass"));
+//		p.clear();
+//		p.sendKeys(password);
+	}
+}
