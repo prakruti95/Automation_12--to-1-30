0a8c1ee0dc04018afef5f13dc70284b99928261a
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-10-15 18:12:31 +0530

    commit

---
+++ tops2/src/main/java/tops1/tops2/App.java
@@ -0,0 +1,13 @@
+package tops1.tops2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
